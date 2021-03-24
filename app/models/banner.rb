2dd2c9236ed6959f21d6d53a1e9c2f4a56bdb83a
taitus <sebastia.roig@gmail.com>
2021-03-24 19:15:53 +0100

    Adding consistency in banner scopes
    Since the :post_started_at and :post_ended_at fields are of type Date, we check
    with Date.current and not with Time.current.
    
    This change has been caused because some test suites were failing when run at midnight (https://github.com/consul/consul/runs/2170798218?check_suite_focus=true).

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -19,9 +19,9 @@ class Banner < ApplicationRecord
   has_many :sections
   has_many :web_sections, through: :sections
 
-  scope :with_active,   -> { where("post_started_at <= ?", Time.current).where("post_ended_at >= ?", Time.current) }
+  scope :with_active,   -> { where("post_started_at <= ?", Date.current).where("post_ended_at >= ?", Date.current) }
 
-  scope :with_inactive, -> { where("post_started_at > ? or post_ended_at < ?", Time.current, Time.current) }
+  scope :with_inactive, -> { where("post_started_at > ? or post_ended_at < ?", Date.current, Date.current) }
 
   scope :in_section, ->(section_name) { joins(:web_sections, :sections).where("web_sections.name ilike ?", section_name) }
 end
