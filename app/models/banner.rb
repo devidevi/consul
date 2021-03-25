790dde07a43aaba059dad87e283f0dc6d362ce37
taitus <sebastia.roig@gmail.com>
2021-03-25 12:00:41 +0100

    Adding consistency between banner scopes
    I made this change due to an alert from github-actions (Layout/LineLength)
    
    I'm not a big fan of reducing the size of lines that I think are clear, but in this case I
    think it's just as clear or more if we apply this change which also gives consistency
    with the :with_inactive scope.

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -19,9 +19,7 @@ class Banner < ApplicationRecord
   has_many :sections
   has_many :web_sections, through: :sections
 
-  scope :with_active,   -> { where("post_started_at <= ?", Date.current).where("post_ended_at >= ?", Date.current) }
-
+  scope :with_active, -> { where("post_started_at <= ? and post_ended_at >= ?", Date.current, Date.current) }
   scope :with_inactive, -> { where("post_started_at > ? or post_ended_at < ?", Date.current, Date.current) }
-
   scope :in_section, ->(section_name) { joins(:web_sections, :sections).where("web_sections.name ilike ?", section_name) }
 end
